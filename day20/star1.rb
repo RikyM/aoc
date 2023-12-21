8a860a84df9323a79c364fd9a2509618a050dcf1
RikyM <RikyM90@gmail.com>
2023-12-21 05:20:47 +0000

    Day20 - Star 1

--- day20/star1.rb
+++ day20/star1.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+BUTTON_PRESSES = 1000
+
 input_file = ARGV[0]
 
 input = File.read(input_file)
@@ -11,17 +13,16 @@ end
 class Gate
   attr_reader :output, :name
 
-  def initialize outputs, name
+  def initialize outputs, name, signals
     @name = name
     @outputs = outputs
-    @outputs = {}
     @inputs = []
+    @signals = signals
   end
 
   def link gates
     @outputs.map! do |out_name|
       gate = gates[out_name]
-      raise 'stocazzo' if gate.nil?
       gate.add_input self
       gate
     end
@@ -32,19 +33,19 @@ class Gate
   end
 
   def emit signal
-    @output = signal
+    @outputs.each {|o| @signals << {source: self.name, target: o, signal: signal}}
   end
 
   def execute source, signal
     raise 'Gate not implemented'
   end
 
-  def send_signal source, signal
-    log "#{source.name} -#{signal ? 'high' : 'low'}-> #{@name}"
+  def execute source, signal
+    raise 'Gate not implemented'
+  end
 
-    if execute(source, signal)
-      @outputs.each{|g| g.send_signal self, @output}
-    end
+  def receive_signal source, signal
+    execute(source, signal)
   end
 end
 
@@ -56,7 +57,7 @@ class Broadcaster < Gate
 end
 
 class FlipFlop < Gate
-  def initialize outputs, name
+  def initialize outputs, name, signals
     super
     @stored = false
   end
@@ -73,17 +74,20 @@ class FlipFlop < Gate
 end
 
 class Conjunction < Gate
-  def initialize outputs, name
+  def initialize outputs, name, signals
     super
     @stored = {}
     @stored.default = false
   end
 
   def execute source, signal
+    @stored[source] = signal
+    output = @inputs.map{|i| @stored[i.name]}.reduce{|a,b| a and b}
+    output = not(output)
 
-    @output = @inputs.all?{|i| @stored[i]}
+    #output = (@stored.size != @inputs.size) and not(@stored.values.reduce(true) {|a,b| a and b})
 
-    emit @output
+    emit output
     return true
 
   end
@@ -91,11 +95,17 @@ class Conjunction < Gate
   
 end
 
-class Module
+class Mod < Gate
+  def initialize name
+    super([], name, nil)
+  end
+
+  def execute souce, signal
+  end
 end
 
-gates = {
-}
+gates = {}
+signals = []
 
 input.each_line do |circuit|
   gate, outputs = circuit.chomp.split(' -> ')
@@ -103,30 +113,44 @@ input.each_line do |circuit|
   gate_type = gate[0]
   gate_name = gate[1..-1].to_sym
   outputs = outputs.split(', ').map(&:to_sym)
-  #log gate_type
-  #log gate_name
-  #log outputs.inspect
+
+  outputs.filter do |name|
+    unless gates.include? name
+        gate = Mod.new name
+        gates[name] = gate
+    end
+  end
 
   case gate_type
   when 'b'
-    gates[:broadcaster] = Broadcaster.new outputs, :broadcaster
+    gates[:broadcaster] = Broadcaster.new outputs, :broadcaster, signals
   when '%'
-    gates[gate_name] = FlipFlop.new outputs, gate_name
+    gates[gate_name] = FlipFlop.new outputs, gate_name, signals
   when '&'
-    gates[gate_name] = Conjunction.new outputs, gate_name
+    gates[gate_name] = Conjunction.new outputs, gate_name, signals
   else
     raise "Unknown gate #{gate_type}"
   end
 end
 
 gates.each_value{|g| g.link gates}
-
 broadcaster = gates[:broadcaster]
-#broadcaster = gates[:a]
 
-1.times do |i|
-  broadcaster.send_signal :button, false
+low = 0
+high = 0
+BUTTON_PRESSES.times do
+  signals << {source: :button, signal: false, target: broadcaster}
+
+  until signals.empty?
+    sig = signals.shift
+    break if sig.nil?
+    if sig[:signal]
+      low += 1
+    else
+      high += 1
+    end
+    sig[:target].receive_signal sig[:source], sig[:signal]
+  end
 end
 
-
-puts -1
+puts low * high
